8a7eccd6845d8701a24fad4385da6896bef8be75
AaronLiebgold <aaron.liebgold@gmail.com>
2021-11-10 09:26:59 -0600

    Script that will change Celcius to Fahrenheit

---
+++ ScriptsForResume/C2F.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# This function will convert Celsius to Fahrenhheit
+
+def Celsius():
+    inputFahrenheit = input("Enter a tempeture to convert to Fahrenheit: ")
+    inputFahrenheitInteger = int(inputFahrenheit)
+    Celsius = (inputFahrenheitInteger - 32) * 5/9
+    print("The tempeture in Fahrenheit is")
+    return(Celsius)
+
+def main():
+    print(Celsius())
+
+if __name__ == "__main__":
+    main()
